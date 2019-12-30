852ab58d5f9a2532b7c4779d4b57d623479b2482
Xander Kehoe <xanmail99@gmail.com>
2019-12-30 14:08:39 -0500

    Cleaned up garbage comments and debugging prints

--- main.py
+++ main.py
@@ -14,13 +14,10 @@ class NeuralNet:
 
         self.weights = []
         for i in range(len(self.actived_nodes) - 1):
-            # print("i:" + str(i))
             self.weights.append([])
             for j in range(len(self.actived_nodes[i + 1])):
-                # print("\tj:" + str(j))
                 self.weights[i].append([])
                 for k in range(len(self.actived_nodes[i])):
-                    # print("\t\tk: " + str(k))
                     self.weights[i][j].append(0)
 
     def randomize_weights(self, scalar):
@@ -46,27 +43,14 @@ class NeuralNet:
     def feed_forward(self, input_layer):
         if len(input_layer) == len(self.actived_nodes[0]):
             self.actived_nodes[0] = input_layer
-            #print("IL: " + str(input_layer))
             for i in range(len(self.actived_nodes) - 1):
-                #print("i:" + str(i))
-                # print("\t\t\t test: " + str(len(self.nodes[i + 1])))
                 for j in range(len(self.actived_nodes[i + 1])):
-                    #print("\tj: " + (str(j)))
-
                     #  tally up sum of weights * inputs to feed forward to next node layer
                     sum = 0;
                     for k in range(len(self.actived_nodes[i])):
-                        #print("\t\tk: " + str(k))
                         sum += self.actived_nodes[i][k] * self.weights[i][j][k]
-                    # try:
                     self.inactive_nodes[i + 1][j] = sum
                     self.actived_nodes[i+1][j] = self.sigmoid(self.inactive_nodes[i+1][j], False)
-                    # except Exception as e:
-                    #     print("CRASH HERE: " + str(e))
-                    #     self.print_nodes()
-                    #     for ii in range(len(weight_list)):
-                    #         print(str(weight_list[ii]))
-                    #     exit(0)
             return self.actived_nodes[len(self.actived_nodes) - 1]
         else:
             print("FEED FORWARD ERROR: input and input layer sizes do not match!")
@@ -102,7 +86,6 @@ class NeuralNet:
                 cost_layer.append((1/2) * (target_layer[i] - output_layer[i]) * (target_layer[i] - output_layer[i]))
 
             return cost_layer
-
         else:
             print("COST LAYER FUNCTION ERROR: target_layer and output_layer sizes do not match!")
             return None
@@ -120,17 +103,12 @@ class NeuralNet:
         propagation_error_temp = []
 
         for i in range(len(self.weights) - 1, -1, -1):
-            #print("i: " + str(i))
             for j in range(len(self.weights[i])):
-                #print("\tj: " + str(j))
                 for k in range(len(self.weights[i][j])):
-                    #print("\t\tk: " + str(k))
-
                     if i == len(self.weights) - 1:
                         new_weights[i][j].append(self.weights[i][j][k] - learning_rate * (-1 * (target_layer[j] - self.actived_nodes[i+1][j]) * NeuralNet.sigmoid(self.inactive_nodes[i+1][j], True) * self.actived_nodes[i][k]))
                         propagation_error.append(-1 * (target_layer[j] - self.actived_nodes[i+1][j]) * NeuralNet.sigmoid(self.inactive_nodes[i+1][j], True) * self.weights[i][j][k])
                     else:
-                        #print(str(propagation_error))
                         new_weights[i][j].append(self.weights[i][j][k] - learning_rate * (propagation_error[j] * NeuralNet.sigmoid(self.inactive_nodes[i+1][j], True) * self.actived_nodes[i][k]))
                         propagation_error_temp.append(propagation_error[j] * self.weights[i][j][k] * NeuralNet.sigmoid(self.inactive_nodes[i+1][j], True))
                         # new_weights[i][j].append(self.weights[i][j][k])  # no change in weights, DEBUGGING ONLY
@@ -146,64 +124,15 @@ train_amount = 10000
 
 nn_1 = NeuralNet(nn_s)
 nn_1.randomize_weights(10)
-# nn_1.weights[0][0][0] = .5
-# nn_1.weights[0][0][1] = .5
-# nn_1.weights[0][1][0] = .5
-# nn_1.weights[0][1][1] = .5
-# nn_1.weights[1][0][0] = .5
-# nn_1.weights[1][0][1] = .5
-#
-# for i in range(100000):
-#     rand = random.randint(1, 2)
-#     if rand == 1:
-#         nn_1.train([0], [1])
-#     if rand == 2:
-#         nn_1.train([1], [0])
-
 
 for j in range(train_amount):
     rand = random.random()
     nn_1.train([rand], [1-rand])
 
-    # rand = random.randint(1, 3)
-    # if rand == 1:
-    #     nn_1.train([0], [1])
-    # if rand == 2:
-    #     nn_1.train([1], [0])
-    # if rand == 3:
-    #     nn_1.train([0.5], [0.5])
-    # if rand == 4:
-    #     nn_1.train([1, 1], [0])
-
     if j % (train_amount * .01) == 0:
         print("Training "+str((j / train_amount) * 100)+"% complete")
         weight_list.append(nn_1.weights)
 
-# for ii in range(len(weight_list)):
-#     print(str(weight_list[ii]))
-
-# print(str(nn_1.feed_forward([0, 0])))
-# print(str(nn_1.feed_forward([0, 1])))
-# print(str(nn_1.feed_forward([1, 0])))
-# print(str(nn_1.feed_forward([1, 1])))
-
 print(str(nn_1.feed_forward([0])))
 print(str(nn_1.feed_forward([0.5])))
 print(str(nn_1.feed_forward([1])))
-
-# nn_1.print_weights()
-# nn_1.print_nodes()
-
-
-# Rand Notes:
-#     to train y = 1-x
-#         nn_s = [1, 2, 3, 2, 1]
-#         for j in range(train_amount):
-#             rand = random.randint(1, 3)
-#             if rand == 1:
-#                 nn_1.train([0], [1])
-#             if rand == 2:
-#                 nn_1.train([1], [0])
-#             if rand == 3:
-#                 nn_1.train([0.5], [0.5])
-
